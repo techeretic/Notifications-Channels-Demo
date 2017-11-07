d14007f7f10fa2dda2a2605d4449522cca87bf44
Prathamesh Shetye <pshetye@atlassian.com>
2017-11-06 22:23:43 -0800

    InboxStyle Notifications

--- app/src/main/java/com/pshetye/notificationschannelsdemo/Channel.kt
+++ app/src/main/java/com/pshetye/notificationschannelsdemo/Channel.kt
@@ -5,7 +5,6 @@ import android.graphics.Color
 import android.support.annotation.ColorInt
 import android.support.annotation.DrawableRes
 import android.support.annotation.StringRes
-import com.pshetye.notificationschannelsdemo.R
 
 enum class Channel(
         val notificationId: Int,
@@ -19,9 +18,14 @@ enum class Channel(
         @DrawableRes val channelNotificationIcon: Int,
         @ColorInt val channelNotificationColor: Int
 ) {
-    DEFAULT(0, NotificationManager.IMPORTANCE_DEFAULT, R.string.channel_default_importance, R.string.channel_default_importance_id, R.string.channel_default_importance_description, R.string.dummy_text_default, R.string.group_channel_default_importance_id, R.string.group_channel_default_importance, R.drawable.ic_check_black_24dp, Color.RED),
     HIGH(1, NotificationManager.IMPORTANCE_HIGH, R.string.channel_high_importance, R.string.channel_high_importance_id, R.string.channel_high_importance_description, R.string.dummy_text_high,  R.string.group_channel_default_importance_id, R.string.group_channel_default_importance, R.drawable.ic_check_black_24dp, Color.GREEN),
     MIN(2, NotificationManager.IMPORTANCE_MIN, R.string.channel_min_importance, R.string.channel_min_importance_id, R.string.channel_min_importance_description, R.string.dummy_text_min,  R.string.group_channel_low_importance_id, R.string.group_channel_low_importance, R.drawable.ic_check_black_24dp, Color.BLUE),
-    LOW(3, NotificationManager.IMPORTANCE_LOW, R.string.channel_low_importance, R.string.channel_low_importance_id, R.string.channel_low_importance_description, R.string.dummy_text_low,  R.string.group_channel_low_importance_id, R.string.group_channel_low_importance, R.drawable.ic_check_black_24dp, Color.WHITE),
+    LOW1(3, NotificationManager.IMPORTANCE_LOW, R.string.channel_low_importance_1, R.string.channel_low_importance_id_1, R.string.channel_low_importance_description_1, R.string.dummy_text_low_1,  R.string.group_channel_low_importance_id, R.string.group_channel_low_importance, R.drawable.ic_check_black_24dp, Color.WHITE),
+    LOW2(4, NotificationManager.IMPORTANCE_LOW, R.string.channel_low_importance_2, R.string.channel_low_importance_id_2, R.string.channel_low_importance_description_2, R.string.dummy_text_low_2,  R.string.group_channel_low_importance_id, R.string.group_channel_low_importance, R.drawable.ic_check_black_24dp, Color.WHITE),
+    LOW3(5, NotificationManager.IMPORTANCE_LOW, R.string.channel_low_importance_3, R.string.channel_low_importance_id_3, R.string.channel_low_importance_description_3, R.string.dummy_text_low_3,  R.string.group_channel_low_importance_id, R.string.group_channel_low_importance, R.drawable.ic_check_black_24dp, Color.WHITE),
+    DEFAULT1(6, NotificationManager.IMPORTANCE_DEFAULT, R.string.channel_default_importance_1, R.string.channel_default_importance_id_1, R.string.channel_default_importance_description_1, R.string.dummy_text_default_1, R.string.group_channel_default_importance_id, R.string.group_channel_default_importance, R.drawable.ic_check_black_24dp, Color.RED),
+    DEFAULT2(7, NotificationManager.IMPORTANCE_DEFAULT, R.string.channel_default_importance_2, R.string.channel_default_importance_id_2, R.string.channel_default_importance_description_2, R.string.dummy_text_default_2, R.string.group_channel_default_importance_id, R.string.group_channel_default_importance, R.drawable.ic_check_black_24dp, Color.RED),
+    DEFAULT3(8, NotificationManager.IMPORTANCE_DEFAULT, R.string.channel_default_importance_3, R.string.channel_default_importance_id_3, R.string.channel_default_importance_description_3, R.string.dummy_text_default_3, R.string.group_channel_default_importance_id, R.string.group_channel_default_importance, R.drawable.ic_check_black_24dp, Color.RED),
+    DEFAULT4(9, NotificationManager.IMPORTANCE_DEFAULT, R.string.channel_default_importance_4, R.string.channel_default_importance_id_4, R.string.channel_default_importance_description_4, R.string.dummy_text_default_4, R.string.group_channel_default_importance_id, R.string.group_channel_default_importance, R.drawable.ic_check_black_24dp, Color.RED);
 //    NONE(4, NotificationManager.IMPORTANCE_NONE, R.string.channel_none_importance, R.string.channel_none_importance_id, R.string.channel_none_importance_description, R.string.dummy_text_none,  R.string.group_channel_low_importance_id, R.string.group_channel_low_importance, R.drawable.ic_check_black_24dp, Color.YELLOW),
 }
--- app/src/main/java/com/pshetye/notificationschannelsdemo/NotificationChannelViewHolder.kt
+++ app/src/main/java/com/pshetye/notificationschannelsdemo/NotificationChannelViewHolder.kt
@@ -10,12 +10,15 @@ import android.support.v4.app.NotificationCompat
 import android.support.v4.app.TaskStackBuilder
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
 class NotificationChannelViewHolder(private val rootView : View) : RecyclerView.ViewHolder(rootView) {
+
+    companion object {
+        private var counter = 0
+    }
     private var channelName : TextView = itemView.findViewById(R.id.channel_name)
     private var notificationText : EditText = itemView.findViewById(R.id.channel_notification_text)
     private var notify : ImageView = itemView.findViewById(R.id.channel_notification_trigger)
@@ -24,6 +27,7 @@ class NotificationChannelViewHolder(private val rootView : View) : RecyclerView.
 
     fun bind(channel: Channel) {
         val channelId = context.getString(channel.channelId)
+        val inboxStyle = NotificationCompat.InboxStyle()
         channelName.setText(channel.channelName)
         notificationText.setText(channel.dummyTestText)
         notificationSettings.setOnClickListener({
@@ -32,20 +36,30 @@ class NotificationChannelViewHolder(private val rootView : View) : RecyclerView.
             i.putExtra(Settings.EXTRA_CHANNEL_ID, channelId)
             context.startActivity(i)
         })
+
+        val builder = NotificationCompat
+                .Builder(context, channelId)
+                .setSmallIcon(channel.channelNotificationIcon)
+                .setContentTitle("Notification")
+                .setAutoCancel(true)
+                .setGroup(context.getString(channel.groupId))
+                .setStyle(inboxStyle)
+
+        var intentText: String = ""
+
         notify.setOnClickListener({
             val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             if (notificationText.text.isEmpty()) {
                 Snackbar.make(rootView, R.string.error_empty_notification_text, Snackbar.LENGTH_LONG).show()
             } else {
-                val mBuilder = NotificationCompat
-                        .Builder(context, channelId)
-                        .setSmallIcon(channel.channelNotificationIcon)
-                        .setContentTitle("My notification")
-                        .setAutoCancel(true)
-                        .setContentText(notificationText.text)
+                val notificationContent = "${notificationText.text} - line $counter"
+                intentText += "\n$notificationContent"
+                inboxStyle.setBigContentTitle(channel.name)
+                inboxStyle.addLine(notificationContent)
+                builder.setContentText(notificationContent).setNumber(++counter)
 
                 val resultIntent = Intent(context, NotifiedActivity::class.java)
-                        .putExtra(NotifiedActivity.Companion.KEY_RESULT, notificationText.text.toString())
+                        .putExtra(NotifiedActivity.Companion.KEY_RESULT, intentText)
 
                 val stackBuilder = TaskStackBuilder.create(context)
                 stackBuilder.addParentStack(MainActivity::class.java)
@@ -56,8 +70,8 @@ class NotificationChannelViewHolder(private val rootView : View) : RecyclerView.
                         PendingIntent.FLAG_UPDATE_CURRENT
                 )
 
-                mBuilder.setContentIntent(resultPendingIntent)
-                notificationManager.notify(channel.notificationId, mBuilder.build())
+                builder.setContentIntent(resultPendingIntent)
+                notificationManager.notify(channel.notificationId, builder.build())
             }
         })
     }
